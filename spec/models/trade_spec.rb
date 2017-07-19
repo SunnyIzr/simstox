860da45bb1047df83c01b210cdf5a66a6cdc3bc2
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2017-07-18 21:18:49 -0400

    Create tests for initial back end associations and validations

--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Trade do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:quantity) }
+  it { should validate_presence_of(:ticker) }
+  it { should validate_presence_of(:price_cents) }
+  it { should validate_presence_of(:portfolio_id) }
+  it { should belong_to(:portfolio)
 end
