86b83cb9ee84f93f69fb9e8d993090dc35d6414f
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2017-07-19 00:33:17 -0400

    Set up relationship between users and trades

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,4 +1,5 @@
 class Trade < ApplicationRecord
   belongs_to :portfolio
+  delegate :user, :to => :portfolio, :allow_nil => true
   validates :quantity, :ticker, :price_cents, :portfolio_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :username, :first_name, :last_name, presence: true
   has_many :portfolios
+  has_many :trades, through: :portfolios
 end
