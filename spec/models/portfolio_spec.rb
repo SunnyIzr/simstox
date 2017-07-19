911a3adb4d849bc0465bb96ee36613db3c51f76c
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2017-07-19 00:10:24 -0400

    Update tests to factor in user model/table

--- spec/models/portfolio_spec.rb
+++ spec/models/portfolio_spec.rb
@@ -3,5 +3,7 @@ require 'spec_helper'
 describe Portfolio do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:cash_cents) }
+  it { should validate_presence_of(:user_id) }
   it { should have_many(:trades) }
+  it { should belong_to(:user) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should have_many(:portfolios) }
 end
