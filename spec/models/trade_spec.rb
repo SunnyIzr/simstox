7962630891cbe49d09cab66029cb388ba2919350
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2017-07-18 21:22:09 -0400

    Fix typo

--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -5,5 +5,5 @@ describe Trade do
   it { should validate_presence_of(:ticker) }
   it { should validate_presence_of(:price_cents) }
   it { should validate_presence_of(:portfolio_id) }
-  it { should belong_to(:portfolio)
+  it { should belong_to(:portfolio) }
 end
