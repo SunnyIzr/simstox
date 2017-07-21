08e9f3464dc0eb136bab45bb65d2eae366f4cb75
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2017-07-20 21:53:55 -0400

    Create models that converts hundreds values to dollars

--- app/models/portfolio_value.rb
+++ app/models/portfolio_value.rb
@@ -1,4 +1,16 @@
 class PortfolioValue < ApplicationRecord
   validates :cash_cents, :market_value_cents, :portfolio_id, :time, presence: true
   belongs_to :portfolio
+
+  def market_value
+    ( market_value_cents / 100.00 ).round(2)
+  end
+
+  def cash
+    ( cash_cents / 100.00 ).round(2)
+  end
+
+  def total_value
+    market_value + cash_cents
+  end
 end
