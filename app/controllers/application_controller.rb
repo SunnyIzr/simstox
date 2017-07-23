d6e9029e98acc03b7bd0af888c41a5e67afb9c2d
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2017-07-23 15:12:57 -0400

    Add authorization methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,32 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
+  
+  def authenticate_request!
+    json_response( {error: 'Permission denied'}, :unauthorized) unless logged_in?
+  end
+
+  def current_user
+    if auth_present?
+      User.find_by(id: auth['user_id'])
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  private
+
+  def auth
+    JsonWebToken.decode(token)
+  end
+
+  def token
+    request.env["HTTP_AUTHORIZATION"]
+  end
+
+  def auth_present?
+    !!auth
+  end
 end
