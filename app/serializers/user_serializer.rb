b24cb28836b7a910daa42cecdd9bc7bb2aeb1caf
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2017-07-23 13:50:31 -0400

    Send back username attribute in user response

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :portfolios
+  attributes :id, :username, :first_name, :last_name, :portfolios
 
   def portfolios
     object.portfolios.collect{ |portfolio| {
