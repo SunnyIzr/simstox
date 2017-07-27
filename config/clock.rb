5d4103ddc0ef559d3e5a910ab421c8533ee7abb7
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2017-07-26 23:52:34 -0400

    Set up clock file to run process at midnight daily

--- config/clock.rb
+++ config/clock.rb
@@ -3,20 +3,11 @@ require './config/boot'
 require './config/environment'
 
 module Clockwork
-  handler do |job|
-    p "Running #{job}"
-    p "*"*100
-    PullQuotesJob.perform_later
+  handler do |job, time|
+    puts "Running My Nightly Batch Process for #{job}, at #{time}"
+    Stock.all.each{ |stock| PullQuotesJob.perform_later(stock)}
+    Portfolio.all.each{ |stock| SavePortfolioValueJob.perform_later(portfolio)}
   end
 
-  # handler receives the time when job is prepared to run in the 2nd argument
-  # handler do |job, time|
-  #   puts "Running #{job}, at #{time}"
-  # end
-
-  every(10.seconds, 'frequent.job')
-  # every(3.minutes, 'less.frequent.job')
-  # every(1.hour, 'hourly.job')
-
-  # every(1.day, 'midnight.job', :at => '00:00')
+  every(1.day, 'midnight.job', :at => '00:00')
 end
