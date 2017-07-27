3718cc798486935739b9f7d69e72f8767bd2c0b0
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2017-07-27 15:07:41 -0400

    Add :email field to user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:first_name, :last_name, :username, :password)
+    params.permit(:first_name, :last_name, :email, :username, :password)
   end
 end
